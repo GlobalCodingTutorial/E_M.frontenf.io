73f8998e645c40426e3201806c01b13cf7647ced
GlobalCodingTutorial <vgola261@gmail.com>
2025-07-03 23:19:11 +0530

    chh

--- src/service/EmployeeService.jsx
+++ src/service/EmployeeService.jsx
@@ -1,24 +1,24 @@
-import axios from 'axios'; 
-const EMPLOYEE_S_APIBASE_URL ="http://codingtutorial.cpqciqgqelz4.eu-north-1.rds.amazonaws.com:3030/employees"
+import axios from "axios";
+const EMPLOYEE_S_APIBASE_URL = "http://localhost:3030/employees";
 class EmployeeService {
-    saveEmployee(employee){
-           return axios.post(EMPLOYEE_S_APIBASE_URL, employee)
-    }
+  saveEmployee(employee) {
+    return axios.post(EMPLOYEE_S_APIBASE_URL, employee);
+  }
 
-    getEmployee(){
-        return axios.get(EMPLOYEE_S_APIBASE_URL)
-    }
+  getEmployee() {
+    return axios.get(EMPLOYEE_S_APIBASE_URL);
+  }
 
-    getEmployeeById(id){
-        return axios.get(EMPLOYEE_S_APIBASE_URL+"/"+id)
-    }
+  getEmployeeById(id) {
+    return axios.get(EMPLOYEE_S_APIBASE_URL + "/" + id);
+  }
 
-    deleteEmployeeById(id){
-        return axios.delete(EMPLOYEE_S_APIBASE_URL+"/"+id)
-    }
-    updateEmployee(employee, id){
-        return axios.put(EMPLOYEE_S_APIBASE_URL+"/"+id,employee)
-    }
+  deleteEmployeeById(id) {
+    return axios.delete(EMPLOYEE_S_APIBASE_URL + "/" + id);
+  }
+  updateEmployee(employee, id) {
+    return axios.put(EMPLOYEE_S_APIBASE_URL + "/" + id, employee);
+  }
 }
 
 export default new EmployeeService();
